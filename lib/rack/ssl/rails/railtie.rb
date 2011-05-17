39464a59ef24fc204d79374efe3be1775fe18884
Jesse Storimer <jstorimer@gmail.com>
2011-05-17 13:41:27 -0400

    Update Railtie for multiple cases

--- lib/rack/ssl/rails/railtie.rb
+++ lib/rack/ssl/rails/railtie.rb
@@ -1,7 +1,7 @@
 class Rack::SSL::Railtie < ::Rails::Railtie
   initializer "middleware" do
-    if config.force_ssl
-      config.middleware.use Rack::SSL
+    if config.respond_to?(:force_ssl) && config.force_ssl
+      Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::SSL
     end
   end
 end
